45f2f5eae25173b4d7cd74236fb437a229bb22c6
ALUNO SENAI PETROLINA <aluno.senaipetrolina@SISTEMAFIEPE.LOCAL>
2026-02-06 08:40:02 -0300

    Alteração no env

--- restaurante-api-demo/backend/src/services/database_connection.js
+++ restaurante-api-demo/backend/src/services/database_connection.js
@@ -2,14 +2,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "restaurante_legal",
-  port: 3308,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
